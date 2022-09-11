c38f1e5903b1f21e4001a7150f12c6fd97f8a9a0
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-09-11 17:26:01 +0000

    Python

---
+++ BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("----Welcome to the Band Game Generator !----\n")
+
+place = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+
+print("\nYour band name could be " + place + " " + pet)
